61b01ff821b864976bdab637eb7b8a5771e2c4b5
Calvin Li <calvin.li.lt@gmail.com>
2013-11-05 19:04:47 -0800

    revert adding WIED case.

--- rss_scrape.py
+++ rss_scrape.py
@@ -232,7 +232,6 @@ if __name__ == '__main__':
                       ],
               "flmd": ["276288", # FTV v. Oppold
                       ],
-              "wied": ["63285",  # Malibu Media v. Doe suit (EFF amicus)
             }
 
     #
